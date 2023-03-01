9ad449424101ab024c9436160c5997df9bce5cc7
unknown <yzhaoinuw@gmail.com>
2023-02-28 23:55:29 -0500

    added more formatting in write_spreadsheet.py; added main() for modules.

---
+++ run_admission_assistant.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 23:46:47 2023
+
+@author: Yue
+"""
+
+import write_spreadsheet
+import write_applicant_records
+
+data_path = r"./ROUND 1 Reviews/"
+
+write_applicant_records.main()
+write_spreadsheet.main()
--- write_applicant_records.py
+++ write_applicant_records.py
@@ -17,35 +17,12 @@ import pandas as pd
 from util_vars import decision_matrix, rename_columns, reviewer_items, selected_cols
 
 
-DATA_PATH = r"./ROUND 1 Reviews/"
-WRITE_PATH = DATA_PATH + "program_data/"
-log_file_path = WRITE_PATH + "errors.log"
-Path(WRITE_PATH).mkdir(parents=True, exist_ok=True)
-
-logger = logging.getLogger("logger")
-logging.getLogger().setLevel(logging.INFO)
-logger.propagate = False
-formatter = logging.Formatter("%(levelname)s - %(message)s")
-file_handler = logging.FileHandler(filename=log_file_path, mode="w")
-file_handler.setFormatter(formatter)
-#file_handler.setLevel(logging.WARNING)
-logger.addHandler(file_handler)
-
-files = [
-    'ROUND 1 - CHINA_He_Hangfeng.xlsx',
-    'ROUND 1 - CHINA_Li_Dongmei.xlsx',
-    'ROUND 1 - CHINA_LUO_Jiebo.xlsx',
-    'ROUND 1 - INDIA_Kahng_Anson.xlsx'
-]
-files = os.listdir(DATA_PATH)
 gpa_pattern = re.compile(r'\d.[\d]+')
-dtype_map = {"GPA 1": str}
 
-#%%
 def process_spreadsheet(excel_file) -> dict:
     reviewer_name = " ".join(excel_file.rstrip(".xlsx").split("_")[1:])
     df = pd.read_excel(excel_file, header=1)
-    df = df.astype("string") # convert all data to string
+    df = df.astype("str") # convert all data to string
     df = df.fillna(np.nan).replace([np.nan], [None]) # replace all nan with None
     df.columns = [header.strip() for header in df.columns] # remove leading and trailing spaces
     df = df.dropna(subset=["Ref"])
@@ -55,27 +32,19 @@ def process_spreadsheet(excel_file) -> dict:
     except KeyError:
         logging.getLogger("logger").exception(f"{excel_file} not parsed successfully, maybe because the reviewer modified some column names.")
         return {}
-    df_sub = df_sub.astype(dtype_map)
-    df_sub["Reviewer Name"] = reviewer_name
-    df_sub["GPA 1"] = df_sub["GPA 1"].apply(lambda x: float(gpa_pattern.match(x).group(0)) if gpa_pattern.match(x) else np.nan)
+
+    df_sub = df_sub.assign(
+        **{"Reviewer Name": reviewer_name,
+         "GPA 1": df_sub["GPA 1"].apply(lambda x: float(gpa_pattern.match(x).group(0)) if gpa_pattern.match(x) else np.nan)
+         }
+    )
     df_sub = df_sub.set_index("Ref")
     return df_sub.to_dict("index")
 
-applicant_records = {}
-for file in files:
-    if not file.endswith(".xlsx"):
-        #print(file)
-        logging.getLogger("logger").critical(f"{file} not in excel format.\n")
-        continue
-    excel_file = DATA_PATH + file
-    logging.info(file)
-    #print(excel_file)
-    record = process_spreadsheet(excel_file)
-    if not record: # if anything went wrong when parsing the excel file, skip
-        continue
+def make_admission_recommendation(record: dict, applicant_records: dict) -> None:
     for applicant_id, data in record.items():
         applicant_name = data["Name"]  
-        
+
         # convert the columns in reviewer_items to lists
         for reviewer_item in reviewer_items:
             data[reviewer_item] = [data[reviewer_item]]
@@ -122,14 +91,47 @@ for file in files:
             applicant_records[applicant_id]["Rating"].append(rating)
             reviewer1_rating = applicant_records[applicant_id]["Rating"][0]
             recommeneded_action = decision_matrix[reviewer1_rating][rating]
-            
+        
+        if recommeneded_action == "Admit":
+            if applicant_records[applicant_id]["Data Structures course"] == "No":
+                recommeneded_action = "Admit - Summer"
         applicant_records[applicant_id]["Recommended Action"] = recommeneded_action
-    
-with open(WRITE_PATH + "applicant_records.json", "w") as outfile1:
-    json.dump(applicant_records, outfile1, indent=2)
 
-handlers = logger.handlers
-for handler in handlers:
-    logger.removeHandler(handler)
-    handler.close()
+def main(data_path=r"./ROUND 1 Reviews/"):
+    WRITE_PATH = data_path + "program_data/"
+    log_file_path = WRITE_PATH + "errors.log"
+    Path(WRITE_PATH).mkdir(parents=True, exist_ok=True)
+    logger = logging.getLogger("logger")
+    logging.getLogger().setLevel(logging.INFO)
+    logger.propagate = False
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    file_handler = logging.FileHandler(filename=log_file_path, mode="w")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    applicant_records = {}
+    files = os.listdir(data_path)
+    for file in files:
+        if not file.endswith(".xlsx"):
+            #print(file)
+            logging.getLogger("logger").critical(f"{file} not in excel format.\n")
+            continue
+        excel_file = data_path + file
+        logging.info(file)
+        #print(excel_file)
+        record = process_spreadsheet(excel_file)
+        if not record: # if anything went wrong when parsing the excel file, skip
+            continue
+        make_admission_recommendation(record, applicant_records)
+        
+    with open(WRITE_PATH + "applicant_records.json", "w") as outfile1:
+        json.dump(applicant_records, outfile1, indent=2)
+    
+    handlers = logger.handlers
+    for handler in handlers:
+        logger.removeHandler(handler)
+        handler.close()
+
+if __name__ == '__main__':
+    main()
        
